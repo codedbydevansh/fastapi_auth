12af81cc73ed3405ff4a33897900a9a6e2313830
Devansh <deevansh346@gmail.com>
2026-06-28 12:56:13 +0530

    Updates

--- email_utils.py
+++ email_utils.py
@@ -10,12 +10,12 @@ conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_FROM"),
-    MAIL_PORT = int(os.getenv("MAIL_PORT", 465)),
+    MAIL_PORT = 587,
     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com"),
-    MAIL_STARTTLS = False, # For Port 587
-    MAIL_SSL_TLS = True, # For Port 587
+    MAIL_STARTTLS = True, # For Port 587
+    MAIL_SSL_TLS = False, # For Port 587
     USE_CREDENTIALS = True,
-    VALIDATE_CERTS = True,
+    VALIDATE_CERTS = False,
     TIMEOUT = 60
 )
 
